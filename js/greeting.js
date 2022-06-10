ac959b41d93c8c21a191d840c6bdd9b03173ae97
Mahabat-Madrahimova <mmadrahimova2005@mail.ru>
2022-06-11 01:11:48 +0600

    dz-2

--- js/greeting.js
+++ js/greeting.js
@@ -1,35 +1,13 @@
-// 1
-const name = prompt('Как вас зовут?');
-alert('Привет! ' + name);
-
-
-// 2
-const arr1 = [1, 2, 3, 4];
-const arr2 = [1, 2, 3, 4];
-
-if (arr1.length > arr2.length) {
-    console.log('arr1 больше чем arr2')
-}else if (arr1.length < arr2.length) {
-    console.log('arr2 больше чем arr1')
-}
-
-// 3
-const color = 'зеленый'
-switch (color) {
-    case 'красный':
-        console.log('проход закрыт');
-        break;
-    case 'желтый':
-        console.log('подождите еще немного')
-        break;
-    case 'зеленый':
-        console.log('проход разрешен')
-        break;
+let a = '*';
+while(a.length<=7){
+    console.log(a);
+    a = a + '*';
 }
 
 
-// 4
-const arr = [[[[[[[[[[[[[[[5]]]]]]]]]]]]]]];
-console.log( arr[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0] );
-
-
+for (let i = 1; i < 101; i++) {
+    if (i % 15 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+}
